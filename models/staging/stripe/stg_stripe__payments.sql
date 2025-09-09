69a1a2a5361f706fa692ec44435ba290e92c706d
WoutervdValk <49402056+WenkyWong@users.noreply.github.com>
2025-09-09 16:04:16 +0200

    add codegen package

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,8 @@
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
-        created as created_date
-    from raw.stripe.payment
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_date
+from {{ source('stripe', 'payment')}}
